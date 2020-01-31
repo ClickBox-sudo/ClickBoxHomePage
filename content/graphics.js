9223e297ed19f54e21c60eceaefa69bc340ffb98
HuanaMari <50596990+HuanaMari@users.noreply.github.com>
2020-01-31 21:54:58 +0100

    css

--- content/graphics.js
+++ content/graphics.js
@@ -1,63 +1,63 @@
-loadGraphic=()=>{
+loadGraphic = () => {
 
-    const graphic={
-        
-                logos:{
-                    fullWhiteLogo:'Click-Box-logo.svg',
-                   cWithBox:'Click-Box-logo1.svg',
-                   cWithBoxBlue:'Click-Box-logo1Blue.svg',
-                   onlyBox:'Click-Box-logoBOX.svg',
-                   lickBox:'Click-Box-logo2.svg',
-                   lick:'Click-Box-logo3.svg',
-                   box:'Click-Box-logo4.svg',
+    const graphic = {
 
-                }
-            
+        logos: {
+            fullWhiteLogo: 'Click-Box-logo.svg',
+            cWithBox: 'Click-Box-logo1.svg',
+            cWithBoxBlue: 'Click-Box-logo1Blue.svg',
+            onlyBox: 'Click-Box-logoBOX.svg',
+            lickBox: 'Click-Box-logo2.svg',
+            lick: 'Click-Box-logo3.svg',
+            box: 'Click-Box-logo4.svg',
+
+        }
+
         ,
-        
-        
-                images:{
-                    codingBckg:'head-bckg-mobile.jpg',
-                    programmer:'programmer.png'
-                },
-                videos:{
-                    keyBoardVideo:'head-back-desktop.mp4',
-                   
-                },
-                icons:{
-                    ukFlag:'uk.svg',
-                    mkFlag:'mk.svg',
-                    alFlag:'al.svg',
-                    leftArrow:'btn-left.png',
-                    rightArrow:'btn-right.png'
 
 
-                },
-                templates:{
-                   food:{
-                            thumbnails:['food.jpg'],
-                            icons:['food-icon.png'],
-                            screenShots:['screen-1.jpg','screen-2.jpg','screen-3.jpg','screen-4.jpg','screen-5.jpg','screen-6.png'],
-                   },
-                   art:{
-                    thumbnails:['art.jpg'],
-                    icons:['art-icon.png'],
-                    screenShots:['screen-1.jpg','screen-2.jpg','screen-3.jpg','screen-4.jpg','screen-5.jpg','screen-6.png'],
-                },             
-           law:{
-            thumbnails:['law.jpg'],
-            icons:['law-icon.png'],
-            screenShots:['screen-1.jpg','screen-2.jpg','screen-3.jpg','screen-4.jpg','screen-5.jpg','screen-6.png'],
-        },             
-   service:{
-    thumbnails:['services.jpg'],
-    icons:['services-icon.png'],
-    screenShots:['screen-1.jpg','screen-2.jpg','screen-3.jpg','screen-4.jpg','screen-5.jpg','screen-6.png','beatySalon.jpg','gallery-1.jpg','mobile-1.jpg','mobile-homepage-1.jpg','mobile-menu-1.jpg'],
-},
-                }
-            
-        
-            }
+        images: {
+            codingBckg: 'head-bckg-mobile.jpg',
+            programmer: 'programmer.png'
+        },
+        videos: {
+            keyBoardVideo: 'head-back-desktop.mp4',
+
+        },
+        icons: {
+            ukFlag: 'uk.svg',
+            mkFlag: 'mk.svg',
+            alFlag: 'al.svg',
+            leftArrow: 'btn-left.png',
+            rightArrow: 'btn-right.png'
+
+
+        },
+        templates: {
+            food: {
+                thumbnails: ['food.jpg'],
+                icons: ['food-icon.png'],
+                screenShots: ['screen-1.jpg', 'screen-2.jpg', 'screen-3.jpg', 'screen-4.jpg', 'screen-5.jpg', 'screen-6.png'],
+            },
+            art: {
+                thumbnails: ['art.jpg'],
+                icons: ['art-icon.png'],
+                screenShots: ['screen-1.jpg', 'screen-2.jpg', 'screen-3.jpg', 'screen-4.jpg', 'screen-5.jpg', 'screen-6.png'],
+            },
+            law: {
+                thumbnails: ['law.jpg'],
+                icons: ['law-icon.png'],
+                screenShots: ['screen-1.jpg', 'screen-2.jpg', 'screen-3.jpg', 'screen-4.jpg', 'screen-5.jpg', 'screen-6.png'],
+            },
+            service: {
+                thumbnails: ['services.jpg'],
+                icons: ['services-icon.png'],
+                screenShots: ['screen-1.jpg', 'screen-2.jpg', 'screen-3.jpg', 'screen-4.jpg', 'screen-5.jpg', 'screen-6.png', 'beatySalon.jpg', 'gallery-1.jpg', 'mobile-1.jpg', 'mobile-homepage-1.jpg', 'mobile-menu-1.jpg'],
+            },
+        }
+
+
+    }
 
     return graphic
 }
